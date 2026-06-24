740008834aaba9b2f1e367945b7e524d69abb32b
Pedro Gadelha <pgpgadelha123@gmail.com>
2026-06-23 21:55:21 -0300

    feat: implementar leitura users no servidor http

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,29 @@
 import http from "node:http";
 
+const users = [
+	{
+		name: "Pedro",
+		status: true,
+	},
+	{
+		name: "Tiken",
+		status: false,
+	},
+	{
+		name: "Tiago",
+		status: true,
+	},
+];
+
 http
-	.createServer(() => console.log("Olá, mundo!"))
+	.createServer((request, response) => {
+		if (request.url === "/users" && request.method === "GET") {
+			response.writeHead(200, { "content-type": "application/json" });
+			response.end(JSON.stringify(users));
+			return;
+		}
+
+		response.writeHead(404, { "content-type": "text/plain" });
+		response.end("Not Found!");
+	})
 	.listen(Number(process.env.PORT));
