0c14a2dd72dd532ebcd00bbb52664a88704ffb16
Pedro Gadelha <pgpgadelha123@gmail.com>
2026-06-30 20:28:50 -0300

    feat: criar mock customer

---
+++ src/mocks/customer.mock.ts
@@ -0,0 +1,22 @@
+import type { Customer } from "../types/types.ts";
+
+export const customers: Customer[] = [
+	{
+		id: 1,
+		name: "Cristiano Ronaldo",
+		email: "cr7@email.com",
+		status: true,
+	},
+	{
+		id: 2,
+		name: "Lionel Messi",
+		email: "leo@email.com",
+		status: true,
+	},
+	{
+		id: 3,
+		name: "Neymar Júnior",
+		email: "ney@email.com",
+		status: false,
+	},
+];
